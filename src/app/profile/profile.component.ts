db44eef862defee1d396c93fbbd388338a0c4294
@abhishek.mohan <abhishek.mohan@surboard.se>
2024-02-04 23:06:03 +0530

    minor fixes

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -3,6 +3,7 @@ import { AuthenticationService } from '../services/authentication.service';
 import { ImageUploadService } from '../services/image-upload.service';
 import { HotToastService } from '@ngneat/hot-toast';
 import { concatMap, switchMap } from 'rxjs';
+import { Router } from '@angular/router';
 // import { ProfileUser } from 'src/app/models/user';
 
 @Component({
@@ -14,7 +15,7 @@ export class ProfileComponent {
 
   user$ = this.authService.currentUser$
 
-  constructor(public authService: AuthenticationService,private is: ImageUploadService, private toast: HotToastService){}
+  constructor(public authService: AuthenticationService,private is: ImageUploadService, private toast: HotToastService,private router:Router){}
 
   uploadImage(event: any, user: any){
     this.is.uploadImage(event.target.files[0], `images/profile/${user.uid}`).pipe(
@@ -27,6 +28,12 @@ export class ProfileComponent {
       switchMap((photoURL: any) => this.authService.updateProfileData({photoURL}))
     ).subscribe();
 
+  }
+
+  logout(){
+    this.authService.logout().subscribe(()=>{
+      this.router.navigate(['/'])
+    })
   }
 
 }
